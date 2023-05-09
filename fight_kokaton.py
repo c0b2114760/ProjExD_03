51629f8268a5f745274ec584297d4e4f02051148
mickey <c0b2114760@edu.teu.ac.jp>
2023-05-09 15:43:12 +0900

    練習３

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -49,6 +49,19 @@ class Bird:
             True, 
             False
         )
+        kk_img0 = pg.transform.rotozoom(pg.image.load(f"ex03/fig/{num}.png"), 0, 2.0)
+        kk_img1 = pg.transform.flip(kk_img0, True, False)  # 右向き, ２倍
+        self._imgs = {
+            (1, 0): kk_img1,   # 右
+            (1, -1): pg.transform.rotozoom(kk_img1, 45, 1.0),   # 右上
+            (0, -1): pg.transform.rotozoom(kk_img1, 90, 1.0),  # 上
+            (-1, -1): pg.transform.rotozoom(kk_img0, -45, 1.0),  # 左上
+            (-1, 0): kk_img0,  # 左
+            (-1, 1): pg.transform.rotozoom(kk_img0, 45, 1.0),  # 左下
+            (0, 1): pg.transform.rotozoom(kk_img1, -90, 1.0),  # 下
+            (1, 1): pg.transform.rotozoom(kk_img1, -45, 1.0)  # 右下
+        }
+        self._img = self._imgs[(+1, 0)]
         self._rct = self._img.get_rect()
         self._rct.center = xy
 
@@ -67,13 +80,18 @@ class Bird:
         引数1 key_lst：押下キーの真理値リスト
         引数2 screen：画面Surface
         """
+        sum_mv = [0, 0]
         for k, mv in __class__._delta.items():
             if key_lst[k]:
                 self._rct.move_ip(mv)
+                sum_mv[0] += mv[0]  # 横方向合計
+                sum_mv[1] += mv[1]  # 縦方向合計
         if check_bound(screen.get_rect(), self._rct) != (True, True):
             for k, mv in __class__._delta.items():
                 if key_lst[k]:
                     self._rct.move_ip(-mv[0], -mv[1])
+        if not (sum_mv[0] == 0 and sum_mv[1] == 0):
+            self._img = self._imgs[tuple(sum_mv)]
         screen.blit(self._img, self._rct)
 
 
